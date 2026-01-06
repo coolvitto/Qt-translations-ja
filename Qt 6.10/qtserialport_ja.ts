d2cc64d0d2bcf0a1524e5d9d129400794d8895ca
coolvitto <160416811+coolvitto@users.noreply.github.com>
2026-01-06 14:55:35 +0900

    Update qtserialport_ja.ts 2026-01-06
    Removed location attributes from multiple messages in the Japanese translation file for QSerialPort.

--- Qt 6.10/qtserialport_ja.ts
+++ Qt 6.10/qtserialport_ja.ts
@@ -4,83 +4,62 @@
 <context>
     <name>QSerialPort</name>
     <message>
-        <location filename="../../qtserialport/src/serialport/qserialport.cpp" line="+25"/>
         <source>No error</source>
         <translation>エラーなし</translation>
     </message>
     <message>
-        <location line="+3"/>
         <source>Device is already open</source>
         <translation>デバイスは既に開かれています</translation>
     </message>
     <message>
-        <location line="+3"/>
         <source>Device is not open</source>
         <translation>デバイスが開かれていません</translation>
     </message>
     <message>
-        <location line="+3"/>
         <source>Operation timed out</source>
         <translation>処理がタイムアウトしました</translation>
     </message>
     <message>
-        <location line="+3"/>
         <source>Error reading from device</source>
         <translation>デバイスから読み込めませんでした</translation>
     </message>
     <message>
-        <location line="+3"/>
         <source>Error writing to device</source>
         <translation>デバイスへ書き込めませんでした</translation>
     </message>
     <message>
-        <location line="+3"/>
         <source>Device disappeared from the system</source>
         <translation>システムからデバイスが消失しました</translation>
     </message>
     <message>
-        <location line="+417"/>
         <source>Unsupported open mode</source>
         <translation>サポートされていないオープンモードです</translation>
     </message>
     <message>
-        <source>The device supports only the ignoring policy</source>
-        <translation type="vanished">このデバイスではデータエラーのポリシーは無視のみサポートしています</translation>
-    </message>
-    <message>
-        <location filename="../../qtserialport/src/serialport/qserialport_unix.cpp" line="+312"/>
         <source>Permission error while creating lock file</source>
         <translation>ロックファイルを生成可能な権限がありません</translation>
     </message>
     <message>
-        <location line="+7"/>
         <source>Permission error while locking the device</source>
         <translation>デバイスをロック可能な権限がありません</translation>
     </message>
     <message>
-        <location line="+284"/>
-        <location line="+57"/>
         <source>Cannot set custom speed for one direction</source>
         <translation>一方向だけ速度をカスタマイズすることはできません</translation>
     </message>
     <message>
-        <location line="-25"/>
         <source>No suitable custom baud rate divisor</source>
         <translation>カスタマイズされたボーレートの除数が適切ではありません</translation>
     </message>
     <message>
-        <location line="+38"/>
-        <location line="+24"/>
         <source>Custom baud rate is not supported</source>
         <translation>ボーレートのカスタマイズはサポートされていません</translation>
     </message>
     <message>
-        <location line="+9"/>
         <source>Invalid baud rate value</source>
         <translation>無効なボーレートの値です</translation>
     </message>
     <message>
-        <location filename="../../qtserialport/src/serialport/qserialport_win.cpp" line="+366"/>
         <source>Custom baud rate direction is unsupported</source>
         <translation>一方向だけボーレートをカスタマイズすることはできません</translation>
     </message>
